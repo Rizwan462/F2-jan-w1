4bc4203313e1de528539a1008d7f9d3a934f12e7
Rizwan <sahilknn462@gmail.com>
2024-01-13 21:45:22 +0530

    f commit

---
+++ script.js
@@ -0,0 +1,58 @@
+/** @format */
+
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+
+  let arr1 = [
+    { id: 4, name: "anyone", age: "27", profession: "deveoper" },
+    { id: 5, name: "for", age: "22", profession: "deveoper" },
+    { id: 6, name: "khan", age: "30", profession: "admn" },
+  ];
+  
+  
+  function PrintDeveloperbyMap() {
+    //Write your code here , just console.log
+    arr.map((dev)=>{
+        if(dev.profession==='developer'){
+          console.log(dev);
+        }
+      });
+    
+  }
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here , just console.log
+
+    arr.forEach((dev)=>{
+        if(dev.profession=='developer'){
+          console.log(dev)
+        }
+      });
+    
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    arr.push({id:4,name:"Rizwan",age:"26",profession:"intern"});
+    console.log(arr);
+  
+
+  }
+  
+  function removeAdmin() {
+    //Write your code here, just console.log
+    arr = arr.filter((dev)=>dev.profession!=='admin');
+  console.log(arr);
+
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    arr1.forEach((dev)=>arr.push(dev));
+
+    console.log(arr);
+  
+  }
